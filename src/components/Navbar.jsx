af8723509b6a693b50863922809cf5b61f0829ff
Daniel Junttila <daniel_junttila@live.se>
2022-12-13 21:52:15 +0100

    add margin to navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,17 +9,17 @@ function Navbar() {
                 <div className="flex items-center flex-shrink-0 text-firstColor mr-20">
                     <h1 className="font-semibold text-3xl tracking-tight">BossMovies</h1>
                 </div>
-                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="w-full block flex-grow md:flex md:items-center md:w-auto">
                     <div className="text-xl md:flex-grow my-5">
-                        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-firstColor mr-4">
+                        <a href="#" className="block mt-4 md:inline-block md:mt-0 text-white hover:text-firstColor mr-8">
                             Movies
                         </a>
-                        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-firstColor">
+                        <a href="#" className="block mt-4 md:inline-block md:mt-0 text-white hover:text-firstColor">
                             Tv-Series
                         </a>
                     </div>
                     <div>
-                        <input type="text" placeholder="Search a movie" className="bg-gray-200 rounded-full text-gray-900 px-4 py-2 focus:outline-none focus:shadow-outline" />
+                        <input type="text" placeholder="Search a movie..." className="bg-gray-200 rounded-full text-gray-900 mt-2 md:mt-0 px-4 py-2 focus:outline-none focus:shadow-outline" />
                     </div>
                 </div>
             </nav>
