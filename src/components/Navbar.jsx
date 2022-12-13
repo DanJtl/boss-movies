6c3594826a71457942d23cea49a60afb65d212cf
Daniel Junttila <daniel_junttila@live.se>
2022-12-13 21:35:55 +0100

    add navbar and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react'
-import { ImSearch } from 'react-icons/im'
-// import 'tailwindcss/dist/tailwind.min.css';
+import React, { useState } from "react";
 
 function Navbar() {
-    const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(true);
 
     return (
         <>
-            <nav className="bg-gray-800 flex items-center justify-between p-4">
-                <a href="#" className="text-white font-bold">
-                    My App
-                </a>
-                <div>
-                    <a href="#" className="text-white hover:text-gray-300 mr-4">
-                        Home
-                    </a>
-                    <a href="#" className="text-white hover:text-gray-300">
-                        About
-                    </a>
+            <nav className="flex items-center justify-between flex-wrap bg-fifthColor p-6">
+                <div className="flex items-center flex-shrink-0 text-firstColor mr-20">
+                    <h1 className="font-semibold text-3xl tracking-tight">BossMovies</h1>
+                </div>
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div className="text-xl md:flex-grow my-5">
+                        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-firstColor mr-4">
+                            Movies
+                        </a>
+                        <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-firstColor">
+                            Tv-Series
+                        </a>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Search a movie" className="bg-gray-200 rounded-full text-gray-900 px-4 py-2 focus:outline-none focus:shadow-outline" />
+                    </div>
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
-export default Navbar
-
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        firstColor: "#CC0001",
+        secondColor: "#AF0000",
+        thirdColor: "#003A6D",
+        fourthColor: "#07264e",
+        fifthColor: "#061735",
+      }
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
