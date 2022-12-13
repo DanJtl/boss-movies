e2143648f19ea21095146905869a1abba9bfde61
Daniel Junttila <daniel_junttila@live.se>
2022-12-13 14:21:04 +0100

    add css folder, Navbar.css and structure to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { ImSearch } from 'react-icons/im'
+import '../css/Navbar.css'
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <>
+            <nav className=''>
+                <div className=''>
+                    <h1 id="logo">BossMovies</h1>
+
+                </div>
+                <input type="text" placeholder='Search a movie...'></input>
+                <ImSearch />
+                <div id="night-switch">
+                    <div id="night-switch-move"></div>
+                </div>
+            </nav>
+        </>
     )
 }
 
