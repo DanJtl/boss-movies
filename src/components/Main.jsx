ee53e27259d5049d8aa5c04c86626248fb33512f
Daniel Junttila <daniel_junttila@live.se>
2022-12-14 13:49:44 +0100

    add useEffect and fetch data with axios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import requests from '../Requests';
 
 function Main() {
     const [movies, setMovies] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(requests.movies);
+                setMovies(response.data.results)
+            } catch (error) {
+                console.log("Something went wrong...");
+            }
+        };
+
+        fetchData();
+        console.log(movies)
+    }, []);
+
+    if (!movies) {
+        return <p>Loading...</p>;
+      }
 
     return (
         <div>Main</div>
