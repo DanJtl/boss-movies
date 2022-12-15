f9fc1020485bb9c46171151b502af95dfe36569b
Daniel Junttila <daniel_junttila@live.se>
2022-12-15 19:40:33 +0100

    add useState and useEffect to fetch data

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React,{ useState, useEffect } from 'react';
 
-function Slider() {
+function Slider({title, fetchURL}) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(fetchURL);
+                setMovies(response.data.results);
+            } catch (error) {
+                console.log("Something went wrong...");
+            }
+        };
+    
+        fetchData();
+    }, [fetchURL]);
+
     return (
-        <div>Slider</div>
-    )
+        <>
+            <h2 className='text-white md:text-xl font-bold'>{title}</h2>   
+            <div className='relative flex items-center'>
+                <div id={'slider'}>
+                    
+                </div>  
+            </div> 
+        </>
+    );
 }
+
 
 export default Slider
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Main from '../components/Main';
 import Slider from '../components/Slider';
-
+import requests from '../Requests';
 
 function Home() {
     return (
         <>
             <Main />
-            <Slider title="Popular" />
+            <Slider title="Popular Movies" fetchURL={requests.popularMovies} />
+            <Slider title="Top Rated Movies" fetchURL={requests.topRatedMovies} />
         </>
     )
 }
